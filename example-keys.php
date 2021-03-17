e0184cab802ab02e966cbed45a28f7c2f71ffb87
Fraserbc <fraserbcprice@gmail.com>
2021-03-17 19:40:01 +0000

    example configs

---
+++ example-keys.php
@@ -0,0 +1,26 @@
+<?php
+// KEYS FILE
+//==========================
+// These keys consist of 16 randomly characters A-Z 0-9, NO SYMBOLS.
+// These API keys are default and you can enter your own, as many as you want.
+// You can pass these API keys to trusted developers to allow them to access the API
+//==========================
+
+$keys = array("yJETqa3BBw5LZZZ1",
+			"WAG5rRn66pzmvQlq",
+			"bWw1C9j0dF5wJdQF",
+			"ApNWMFp6mINeOYnd",
+			"vWKxwhjwF2nTzexJ",
+			"SyIoQxs0JQnYcz0X",
+			"vdXSXC7YDCDH1G08",
+			"I0dNQlFt7RPEEKh0",
+			"I4pEeEpa19ejWTM7",
+			"D1erX19gRKtEFNJd",
+			"su0cPTI7O6Uj6wUn",
+			"LS1VK742O9JIYPIJ",
+			"SA6hy7S7I2EmqM8j",
+			"ZLtUzeCoY557GQ5B",
+			"JhCmDBx66XAvLK2q",
+			"3SI871iNWsAd8pPD");
+?>
+?>
